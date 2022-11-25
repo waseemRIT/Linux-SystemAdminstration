bbc00b095cf8b74ca532d51c34bd29a52558ad67
waseemRIT <89963412+waseemRIT@users.noreply.github.com>
2022-11-25 16:40:20 +0400

    Update sym_link.py

--- sym_link.py
+++ sym_link.py
@@ -2,17 +2,15 @@
 import os
 import subprocess
 
-
 # STUDENT: WASEEM QAFFAF
 # STUDENT ID: whq8052
 # DATE 21/11/2022
 
 # INFORMING USER OF THEIR CURRENT PATH
+print("YOU ARE IN:")
 print(os.getcwd())
 # PATH OF THE HOME DIRECTORY, DEFINED GLOBALLY SINCE IT WILL BE THE SAME FOR ALL
 home = os.path.expanduser("~")
-# MOVES TO THE HOME DIRECTORY
-os.chdir(home)
 
 
 def create_symlink():
@@ -32,13 +30,13 @@ def create_symlink():
     file_path = input("Insert Name/Path of file you want to create a shortcut/Symbolic Link for:\n")
     # checks if the file exists or not
     if os.path.exists(file_path):
-        name_of_symlink = input("Insert Name of the shortcut/symbolic link:\n")
+        name_of_symlink = input("Insert Name of the shortcut/symbolic link:\n--> ")
         # Joins the Shortcut with its full path to the hone directory
         name_of_symlink = os.path.join(home, name_of_symlink)
         # ln -s file_path symlink_name
         # Command to create the Symlink/Shortcut
         os.symlink(file_path, name_of_symlink)
-    else: # NOTIFY USER IN CASE FILE DOESN'T EXIST
+    else:  # NOTIFY USER IN CASE FILE DOESN'T EXIST
         print("User File Does Not Exist!")
 
 
@@ -50,36 +48,58 @@ def delete_symlink():
     """
     symlink_to_delete = input("Insert Path to Symlink you want to remove:\n")
 
-    x = subprocess.run(
-        ["rm", symlink_to_delete],
-        stdout=subprocess.PIPE
-    )
-    if x.returncode == 0:
-        print(f"{symlink_to_delete} deleted successfully !")
+    try:
+        os.unlink(symlink_to_delete)  # TO UNLINK THE SYMLINK
+        print(f"{symlink_to_delete} deleted successfully !")  # DISPLAY TO USER THAT IT IS UNLINKED IS SUCCESSFULLY
+    except FileNotFoundError():  # JUST IN CASE USER GIVES A NON-EXISTENT FILE, NOTIFYING USER
+        print(f"{symlink_to_delete} doesn't exist\nPlease Insert A Valid SymLink")
 
 
 def display_summary():
-    subprocess.run(
-        ["sudo", home, "-type", "l"],
-        stdout=subprocess.PIPE
+    """
+    Function to Display a Summary of Linked Files
+    Displays the number of Linked Files
+    Displays the name of the File
+    Path of the Link
+    """
+    output = subprocess.Popen(  # To Find the List of Linked Files
+        ["sudo", "find", home, "-type", "l"],
+        stdout=subprocess.PIPE,
+        text=True
     )
-    pass
+    counter = 0  # TO KEEP A COUNTER OF THE NUMBER OF FILES
+    stdout, stderr = output.communicate()  # TO GET THE OUTPUT OF THE CODE
+    for line in stdout.splitlines():  # SPLITTING THE LINES SO WE COULD LOOP OVER THEM
+        fileName = line.split("\\")  # TO GET THE NAME OF THE FILE SPLIT INTO A LIST
+        print(f"Linked File Name is: {fileName[-1]}")  # PRINTING LAST INDEX OF LIST SINCE LAST INDEX IS THE NAME OF
+        # FILE
+        print(f"Path to {fileName[-1]} is:")
+        print(line)  # PRINTS THE FULL PATH TO THE LINKED FILE
+        counter += 1  # INCREMENTING THE COUNTER OF LINKKED FILES
+    print(f"\nTHE NUMBER OF SYMLINKS AVAILABLE IS {counter}")
 
 
 def main():
+    """
+    FUNCTION TO LIST 4 OPTIONS
+    1 -> TO GO TO CREATING A SYMLINK FUNCTION
+    2 -> TO GO TO DELETING A SYMLINK FUNCTION
+    2 -> TO GO TO SUMMARY FUNCTION
+    QUIT -> TO EXIT THE PROGRAM
+    """
     user_option = input("Please insert one of the following options:\n1 to add a shortcut\n2 to remove a shortcut\n3 "
-                        "to get a summary of shortcuts in the home\nquite to stop execution of the program\n--> ")
+                        "to get a summary of shortcuts in the home\nquit to stop execution of the program\n--> ")
     user_option.lower()  # incase someone inserts eXit capital litters for the exit option!!
     if user_option == "1":
         # create shortcut/symbolic link Function
-        pass
+        create_symlink()
     elif user_option == "2":
         # delete shortcut/symbolic link Function
-        pass
+        delete_symlink()
     elif user_option == "3":
         # Display summary function
-        pass
-    elif user_option == "quite":
+        display_summary()
+    elif user_option == "quit":
         # To stop the execution of the program
         exit()
     else:
@@ -87,7 +107,6 @@ def main():
 
 
 if __name__ == '__main__':
-    # os.system("clear")
+    os.system("clear")  # TO CLEAR THE SCREEN IN THE  BEGGING OF THE PROGRAM
     while True:
-        # main()
-        pass
+        main()
