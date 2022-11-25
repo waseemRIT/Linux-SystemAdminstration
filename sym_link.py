a84a6d041ad9b8e69e1653a0adf9460bf5fc1f23
waseemRIT <89963412+waseemRIT@users.noreply.github.com>
2022-11-25 21:06:38 +0400

    Update sym_link.py

--- sym_link.py
+++ sym_link.py
@@ -65,17 +65,17 @@ def display_summary():
     output = subprocess.Popen(  # To Find the List of Linked Files
         ["sudo", "find", home, "-type", "l"],
         stdout=subprocess.PIPE,
-        text=True
+        universal_newlines=True
     )
     counter = 0  # TO KEEP A COUNTER OF THE NUMBER OF FILES
     stdout, stderr = output.communicate()  # TO GET THE OUTPUT OF THE CODE
     for line in stdout.splitlines():  # SPLITTING THE LINES SO WE COULD LOOP OVER THEM
-        fileName = line.split("\\")  # TO GET THE NAME OF THE FILE SPLIT INTO A LIST
+        fileName = line.split("/")  # TO GET THE NAME OF THE FILE SPLIT INTO A LIST
         print(f"Linked File Name is: {fileName[-1]}")  # PRINTING LAST INDEX OF LIST SINCE LAST INDEX IS THE NAME OF
         # FILE
         print(f"Path to {fileName[-1]} is:")
         print(line)  # PRINTS THE FULL PATH TO THE LINKED FILE
-        counter += 1  # INCREMENTING THE COUNTER OF LINKKED FILES
+        counter += 1  # INCREMENTING THE COUNTER OF LINKED FILES
     print(f"\nTHE NUMBER OF SYMLINKS AVAILABLE IS {counter}")
 
 
@@ -110,3 +110,4 @@ if __name__ == '__main__':
     os.system("clear")  # TO CLEAR THE SCREEN IN THE  BEGGING OF THE PROGRAM
     while True:
         main()
+
