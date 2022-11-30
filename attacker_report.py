818aa57e345db14181fd0c4243d0577883874fed
waseemRIT <89963412+waseemRIT@users.noreply.github.com>
2022-12-01 00:37:39 +0400

    set up

---
+++ attacker_report.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# STUDENT: WASEEM QAFFAF
+# STUDENT ID: whq8052
+# DATE 2/12/2022
+
+from geoip import geolite2
+import os
+
+
+
+def main():
+    os.system("clear")
+
+
+if __name__ == '__main__':
+    main()
