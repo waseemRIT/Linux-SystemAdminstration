fcf1637e8c10495adc96ee765160ef4e35a6de4f
waseemRIT <89963412+waseemRIT@users.noreply.github.com>
2022-12-01 19:16:26 +0400

    Extracted IPS

--- attacker_report.py
+++ attacker_report.py
@@ -2,14 +2,27 @@
 # STUDENT: WASEEM QAFFAF
 # STUDENT ID: whq8052
 # DATE 2/12/2022
+import re
 
 from geoip import geolite2
 import os
 
+myAuthlog = open('syslog.log', 'r')  # open the syslog.log for reading
+
+ips_list = []
+
+for line in myAuthlog:
+    x = re.search("(\d{1,3}\.){3}\d{1,3}", line)
+    if x:
+        ips_list.append(x.group())
+
+print(ips_list)
+
 
 
 def main():
-    os.system("clear")
+    # os.system("clear")
+    pass
 
 
 if __name__ == '__main__':
