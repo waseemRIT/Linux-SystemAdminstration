216f974db9d64d7c22cc6f0d91cc4df30938aafd
waseemRIT <89963412+waseemRIT@users.noreply.github.com>
2022-12-01 19:54:05 +0400

    Update attacker_report.py

--- attacker_report.py
+++ attacker_report.py
@@ -6,18 +6,33 @@ import re
 
 from geoip import geolite2
 import os
+import ipaddress
 
 myAuthlog = open('syslog.log', 'r')  # open the syslog.log for reading
 
 ips_list = []
+# with open("syslog.log") as f:
+#    data = f.read()
+#
+# address_ipv4 = re.findall(r"\b(\d{1,3}\.){3}\d{1,3}\b", data)
+#
+# print(address_ipv4)
 
-for line in myAuthlog:
-    x = re.search("(\d{1,3}\.){3}\d{1,3}", line)
+
+for line in myAuthlog:  # loop to extract the IP address
+    x = re.search("((\d\d?|[01]\d\d|2([0-4]\d|5[0-5]))\.){3}(\d\d?|[01]\d\d|2([0-4]\d|5[0-5]))", line)
     if x:
         ips_list.append(x.group())
 
-print(ips_list)
+ips_set = set(ips_list)  # removes duplicates
 
+for unique_ip in ips_set:
+    counter = 0
+    for ip in ips_list:
+        if ip == unique_ip:
+            counter += 1
+    if counter >= 10:
+        print(f"{unique_ip} is repeated {counter} times")
 
 
 def main():
